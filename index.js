1b33688e0210afef166ac044eaf296fdd9ce8bf8
lyne <me@lyne.io>
2019-10-06 00:38:42 +0200

    Small console style fix

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const config = require('./config');
 const manager = new ShardingManager('./bot.js', { token: config.discord.token });
 
 manager.spawn(config.discord.shards);
-manager.on('launch', shard => console.log(`Launched shard ${shard.id}`));
+manager.on('launch', shard => console.log(`Launched shard #${shard.id}`));
