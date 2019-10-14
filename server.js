98ad082f71f7ec3838f704972d28f27efc1c7014
lyne <me@lyne.io>
2019-10-14 21:15:10 +0200

    Made server port configurable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const config = require('./config')
+const port = config.server.port
 
 module.exports.run = async () => {
   try {
