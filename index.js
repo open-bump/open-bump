6ee53892b902421b2ee8e93f6b9115105a977e21
lyne <me@lyne.io>
2019-10-15 21:23:36 +0200

    Manager shard startup logging bugfix

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ String.prototype.replaceAll = (str, search, replacement) => {
   return str && str.replace ? str.replace(new RegExp(search, 'g'), replacement) : str
 }
 
-manager.spawn(config.discord.shards)
 manager.on('launch', shard => console.log(`Launched shard #${shard.id}`))
+manager.spawn(config.discord.shards)
 
 // Database
 mongoose.connect(''.replaceAll(config.database.mongoURI, '%database%', config.database.database), {
