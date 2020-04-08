9a32ffcc606bb3cdafaab67938af91f1eda39db6
Lars S. Peder <lars@peder.cc>
2020-04-08 22:01:24 +0200

    Start sequelize setup

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,18 @@
 import config from "config";
 
 interface IConfig extends config.IConfig {
+  database:
+    | {
+        dialect: "mysql";
+        host: string;
+        database: string;
+        username: string;
+        password: string;
+      }
+    | {
+        dialect: "sqlite";
+        storage: string;
+      };
   discord: {
     token: string;
   };
