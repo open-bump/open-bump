dd3f0d9794abe708b8263db88306a802d974395e
lyne <me@lyne.io>
2020-04-11 12:06:39 +0200

    Test bump command

---
+++ src/commands/BumpCommand.ts
@@ -0,0 +1,21 @@
+import { ParsedMessage } from "discord-command-parser";
+import Command from "../Command";
+import Guild from "../models/Guild";
+import Utils from "../Utils";
+
+export default class BumpCommand extends Command {
+  public name = "bump";
+  public syntax = "bump";
+  public description = "Bump your server";
+
+  public async run({ message }: ParsedMessage, guildDatabase: Guild) {
+    const { channel, guild } = message;
+
+    await channel.send("test...");
+
+    const embed = await Utils.Bump.getEmbed(guild, guildDatabase);
+    const amount = await Utils.Bump.bumpToThisShard(guildDatabase, embed);
+
+    await channel.send(`bumped: ${amount}`);
+  }
+}
