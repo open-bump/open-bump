d6ff92686991baa8b51d900778b999d93c1dec92
Lars S. Peder <lars@peder.cc>
2020-04-11 13:02:16 +0200

    Start hub

--- hub/src/Hub.ts
+++ hub/src/Hub.ts
@@ -1,21 +1,25 @@
 import DatabaseManager from "./DatabaseManager";
+import ServerManager from "./ServerManager";
 
-export default class OpenBump {
-  public static instance: OpenBump;
+export default class Hub {
+  public static instance: Hub;
 
   public directory = __dirname;
 
   public databaseManager: DatabaseManager;
+  public serverManager: ServerManager;
 
   constructor() {
-    OpenBump.instance = this;
+    Hub.instance = this;
 
     this.databaseManager = new DatabaseManager(this);
+    this.serverManager = new ServerManager(this);
 
     this.init();
   }
 
   private async init() {
     await this.databaseManager.init();
+    await this.serverManager.init();
   }
 }
