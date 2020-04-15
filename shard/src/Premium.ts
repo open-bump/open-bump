e9cc4095ebb98a175b2a7db7821d8e453b877783
lyne <me@lyne.io>
2020-04-15 20:05:02 +0200

    WIP: Premium

--- shard/src/Premium.ts
+++ shard/src/Premium.ts
@@ -4,6 +4,9 @@ import AssignedTier from "./models/AssignedTier";
 import Donator from "./models/Donator";
 import PremiumTier from "./models/PremiumTier";
 import OpenBump from "./OpenBump";
+import Utils from "./Utils";
+import config from "./config";
+import moment from "moment";
 
 export default class Premium {
   constructor(private instance: OpenBump) {}
@@ -15,34 +18,119 @@ export default class Premium {
   private async premiumLoop() {
     console.log("Running premium loop...");
 
-    const donators = await Donator.findAll({
-      attributes: [
-        "userId",
-        [
-          Sequelize.fn("SUM", Sequelize.col("assignedTiers.premiumTier.cost")),
-          "totalAssigned"
-        ],
-        [Sequelize.literal('patreon + bonus'), 'totalBalance']
-      ],
+    const violators = await Donator.findAll({
+      attributes: {
+        include: [
+          [
+            Sequelize.fn(
+              "SUM",
+              Sequelize.col("assignedTiers.premiumTier.cost")
+            ),
+            "totalAssigned"
+          ],
+          [Sequelize.literal("patreon + bonus"), "totalBalance"]
+        ]
+      },
       group: "userId",
-      include: [{ model: AssignedTier, attributes:['premiumTierId'], required: true, include: [{ model: PremiumTier.scope(''), attributes: ['cost'], required: true }] }],
+      include: [
+        {
+          model: AssignedTier,
+          attributes: ["premiumTierId"],
+          required: true,
+          include: [
+            {
+              model: PremiumTier.scope(""),
+              attributes: ["cost"],
+              required: true
+            }
+          ]
+        }
+      ],
       where: {
         "$assignedTiers.premiumTier.cost$": {
           [Op.gt]: 0
-        },
+        }
+      },
+      having: {
+        totalAssigned: {
+          [Op.gt]: Sequelize.col("totalBalance")
+        }
       },
       logging: console.log
     });
 
-    console.log(
-      donators.map((donator) => `${donator.userId}:${donator.get('totalAssigned')}/${donator.get("totalBalance")}`)
-    );
+    for (const violator of violators) {
+      if (!violator.transitionStartedAt) {
+        // No transition started yet
+        violator.set("transitionStartedAt", new Date());
 
-    // TODO: Current position.
-    //       Start transition and inform donators with insufficient balance.
-    //       The query above needs to be improved as it only is for testing purposes.
-    //       The total balance the user has currently is null when either patreon or bonus is null.
-    //       A migration that sets all null values and the default value to 0, fixes that.
+        try {
+          const user = await this.instance.client.users.fetch(violator.userId);
+          const embed = {
+            color: Utils.Colors.RED,
+            title: `${Utils.Emojis.XMARK} Payment issue`,
+            description:
+              "Hey there, we recently detected a problem with your premium. " +
+              "It looks like your balance is not enough to cover the cost for all activated servers. " +
+              "Please fix this issue asap." +
+              "You can increate your pledge or disable/change change servers. " +
+              "To get an overview of your activated premium servers, use the command `ob!premium` to do so. " +
+              "Please note, the premium command needs to be executed on your own server and not via DMs. " +
+              `If you believe this is an error, please contact **[Support](${config.settings.support})**.`
+          };
+          await user.send({ embed });
+          violator.transitionStartInformed = true;
+        } catch (error) {
+          violator.transitionStartInformed = false;
+        }
+        await violator.save();
+      } else if (violator.transitionStartedAt) {
+        if (
+          moment(violator.transitionStartedAt).isBefore(
+            moment().subtract(1, "m")
+          )
+        ) {
+            // Transition due
+
+            // TODO: Current position.
+            //       Handle due transitions.
+          } else if (!violator.transitionStartInformed) {
+          // Transition not due, but not yet informed
+          try {
+            const user = await this.instance.client.users.fetch(
+              violator.userId
+            );
+            const embed = {
+              color: Utils.Colors.RED,
+              title: `${Utils.Emojis.XMARK} Payment issue`,
+              description:
+                "Hey there, we recently detected a problem with your premium. " +
+                "It looks like your balance is not enough to cover the cost for all activated servers. " +
+                "Please fix this issue asap." +
+                "You can increate your pledge or disable/change change servers. " +
+                "To get an overview of your activated premium servers, use the command `ob!premium` to do so. " +
+                "Please note, the premium command needs to be executed on your own server and not via DMs. " +
+                `If you believe this is an error, please contact **[Support](${config.settings.support})**.`
+            };
+            await user.send({ embed });
+            violator.transitionStartInformed = true;
+          } catch (error) {
+            violator.transitionStartInformed = false;
+          }
+          if (violator.changed()) await violator.save();
+        }
+      }
+    }
+
+    // Only for debug reasons
+    console.log(
+      violators.map(
+        (violator) =>
+          `${violator.userId}:${violator.get("totalAssigned")}/${violator.get(
+            "totalBalance"
+          )}`
+      )
+    );
 
     setTimeout(this.premiumLoop.bind(this), 1000 * 60);
   }
