b8ed92bf666ab81585c531dd8ad1c6caa0bb3a70
lyne <me@lyne.io>
2020-05-25 21:45:03 +0200

    Bump badges

--- shard/src/Utils.ts
+++ shard/src/Utils.ts
@@ -136,7 +136,7 @@ class Bump {
     const region = regions.get(guild.region);
 
     // Description
-    const description =
+    let description =
       `${Utils.Emojis.OWNER} **Owner:** ${guild.owner?.user.tag}\n` +
       `${Utils.Emojis.REGION} **Region:** ${region?.name}\n` +
       `${Utils.Emojis.CREATED} **Created:** ${ms(
@@ -149,6 +149,26 @@ class Bump {
     // Author
     if (!authorId) authorId = String(OpenBump.instance.client.user?.id);
     const author = await OpenBump.instance.client.users.fetch(authorId);
+
+    // Badges
+    const features = guildDatabase.getFeatures();
+    const badges = [];
+    if (features.includes(Utils.Feature.SUPPORT_SERVER))
+      badges.push(Utils.Emojis.SUPPORT_SERVER);
+    if (guild.verified) badges.push(Utils.Emojis.VERIFIED_SERVER);
+    if (guild.partnered) badges.push(Utils.Emojis.PARTNERED_SERVER);
+    if (features.includes(Utils.Feature.FEATURED))
+      badges.push(Utils.Emojis.FEATURED);
+    if ((guild.premiumSubscriptionCount || 0) >= 30)
+      badges.push(Utils.Emojis.BOOST_3);
+    else if ((guild.premiumSubscriptionCount || 0) >= 15)
+      badges.push(Utils.Emojis.BOOST_2);
+    else if ((guild.premiumSubscriptionCount || 0) >= 2)
+      badges.push(Utils.Emojis.BOOST_1);
+
+    if (badges.length) {
+      description = badges.join(" ") + `\n\n` + description;
+    }
 
     // Create
     return {
@@ -883,7 +903,8 @@ export default class Utils {
     FEATURED: "FEATURED" as "FEATURED",
     CROSS: "CROSS" as "CROSS",
     RESTRICTED_CHANNEL: "RESTRICTED_CHANNEL" as "RESTRICTED_CHANNEL",
-    AUTOBUMP: "AUTOBUMP" as "AUTOBUMP"
+    AUTOBUMP: "AUTOBUMP" as "AUTOBUMP",
+    SUPPORT_SERVER: "SUPPORT_SERVER" as "SUPPORT_SERVER"
   };
 
   public static Emojis = {
@@ -926,6 +947,12 @@ export default class Utils {
     EARLYSUPPORTER: "<:EarlySupporter:622852038031835137>",
     AFFILIATEDSERVER: "<:AffiliatedServer:622857526924279848>",
     BUMPCHANNEL: "<:BumpChannel:632703590632390686>",
+    SUPPORT_SERVER: "<:official_support:707638686724128880>",
+    VERIFIED_SERVER: "<:verified:707638692885430422>",
+    PARTNERED_SERVER: "<:partner:707638685742661714>",
+    BOOST_3: "<:boost_3:707638684555411578>",
+    BOOST_2: "<:boost_2:707638684530507907>",
+    BOOST_1: "<:boost_1:707638684358410271>",
     getRaw: (emoji: string) => {
       const regex = /<a?:.{0,}:([0-9]{10,20})>/gim;
       let m;
---
+++ shard/src/commands/BadgesCommand.ts
@@ -0,0 +1,35 @@
+import { SuccessfulParsedMessage } from "discord-command-parser";
+import { MessageEmbedOptions } from "discord.js";
+import Command from "../Command";
+import Guild from "../models/Guild";
+import Utils, { GuildMessage } from "../Utils";
+
+export default class BadgesCommand extends Command {
+  public name = "badges";
+  public aliases = ["badge"];
+  public syntax = "badges";
+  public description = "View all available badges";
+
+  public async run(
+    { message, arguments: args }: SuccessfulParsedMessage<GuildMessage>,
+    _guildDatabase: Guild
+  ) {
+    const { channel } = message;
+
+    const embed: MessageEmbedOptions = {
+      title: `Bump Badges`,
+      description:
+        `${Utils.Emojis.SUPPORT_SERVER} - Official Bot Support Server\n` +
+        `${Utils.Emojis.VERIFIED_SERVER} - Verified Discord Servers\n` +
+        `${Utils.Emojis.PARTNERED_SERVER} - Discord Partnered Server\n` +
+        `${Utils.Emojis.FEATURED} - Featured Server\n` +
+        `${Utils.Emojis.BOOST_3} - Server Boosted Level 3\n` +
+        `${Utils.Emojis.BOOST_2} - Sever Boosted Level 2\n` +
+        `${Utils.Emojis.BOOST_1} - Server Boosted Level 1`,
+      thumbnail: {
+        url: this.instance.client.user?.displayAvatarURL()
+      }
+    };
+    await channel.send({ embed });
+  }
+}
