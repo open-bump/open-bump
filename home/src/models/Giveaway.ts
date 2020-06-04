f73914323cf6b45cac72617f2ce7406c72a5b0f1
lyne <me@lyne.io>
2020-06-04 23:27:32 +0200

    Giveaways models and beginning of logic

---
+++ home/src/models/Giveaway.ts
@@ -0,0 +1,54 @@
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  ForeignKey,
+  HasMany,
+  Model,
+  PrimaryKey,
+  Table
+} from "sequelize-typescript";
+import GiveawayRequirement from "./GiveawayRequirement";
+import Guild from "./Guild";
+
+@Table({
+  tableName: "Giveaway"
+})
+export default class Giveaway extends Model<Giveaway> {
+  // Discord Snowflake (Message)
+  @PrimaryKey
+  @AllowNull(false)
+  @Column(DataType.STRING(20))
+  id!: string;
+
+  @ForeignKey(() => Guild)
+  @AllowNull(false)
+  @Column
+  guildId!: string;
+
+  @BelongsTo(() => Guild)
+  guild!: Guild;
+
+  @AllowNull(false)
+  @Column(DataType.STRING(20))
+  channel!: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  prize!: string;
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  time!: number;
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  winnersCount!: number;
+
+  @HasMany(() => GiveawayRequirement)
+  requirements!: Array<GiveawayRequirement>;
+
+  @Column(DataType.DATE)
+  endedAt?: Date;
+}
