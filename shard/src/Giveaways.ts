ebe7bef0c6ca2d581ef0d55464f2a6e2f9192aed
lyne <me@lyne.io>
2020-06-06 17:10:57 +0200

    Giveaway cancel command

--- shard/src/Giveaways.ts
+++ shard/src/Giveaways.ts
@@ -19,7 +19,7 @@ export default class Giveaways {
     time: number,
     winnersCount: number,
     requirements: Array<RequirementData> = []
-  ) {
+  ): Promise<Giveaway> {
     const message = await channel.send(`Loading giveaway...`);
 
     const giveaway = await Giveaway.create({
@@ -49,8 +49,39 @@ export default class Giveaways {
       await message.edit("", { embed });
     } else {
       await message.delete();
-      // TODO: Error
+      return giveaway; // TODO: Error
     }
+
+    await message.react(Utils.Emojis.getRaw(Utils.Emojis.TADA));
+
+    return giveaway;
+  }
+
+  public static async cancel(
+    giveaway: string | Giveaway,
+    guildId: string,
+    authorId: string
+  ): Promise<Giveaway> {
+    if (typeof giveaway === "string")
+      giveaway = (await Giveaway.findOne({
+        where: { id: giveaway }
+      })) as Giveaway;
+    if (!giveaway) throw new Error(); // TODO: Error giveaway not found
+    if (giveaway.guildId !== guildId) throw new Error(); // TODO: Error giveaway not on this guild
+    const guild = OpenBump.instance.client.guilds.cache.get(giveaway.guildId);
+    if (!guild) throw new Error(); // TODO: Error guild not found
+    const channel = guild.channels.cache.get(
+      giveaway.channel
+    ) as TextBasedGuildChannel;
+    if (channel?.type !== "text" && channel?.type !== "news") throw new Error(); // TODO: Channel invalid type
+    if (!channel) throw new Error(); // TODO: Error channel not found
+    const message = await channel.messages.fetch(giveaway.id);
+    if (!message) throw new Error(); // TODO: Error message not found
+    giveaway.cancelledBy = authorId;
+    const embed = await this.giveawayToEmbed(giveaway);
+    await message.edit({ embed });
+    await giveaway.save();
+    return giveaway;
   }
 
   public static async giveawayToEmbed(
@@ -61,43 +92,55 @@ export default class Giveaways {
       guild = OpenBump.instance.client.guilds.cache.get(giveaway.guildId);
     if (!guild) throw new Error(); // TODO: Error
 
-    let description = [`React with ${Utils.Emojis.TADA} to enter!`];
+    const endsAt = new Date(giveaway.createdAt).valueOf() + giveaway.time;
 
-    if (giveaway.winnersCount !== 1)
-      description.push(`Winners: **${giveaway.winnersCount}**`);
+    let description = [];
 
-    for (const requirement of giveaway.requirements) {
-      if (requirement.type === "GUILD") {
-        const requirementGuildDatabase = await Guild.findOne({
-          where: { id: requirement.target as string }
-        });
-        if (!requirement) return; // TODO: Throw error
-        description.push(
-          `Must join: **[${requirementGuildDatabase?.name}](${requirement.invite})**`
-        );
-      } else if (requirement.type === "ROLE") {
-        const role = guild.roles.fetch(requirement.target);
-        if (!role) return; // TODO: Throw error
-        description.push(`Must have role: ${role}`);
-      } else if (requirement.type === "VOTE")
-        description.push(
-          `Must vote for: **[${
-            OpenBump.instance.client.user?.username
-          }](${Utils.Lists.getLinkTopGG()})**`
-        );
-      else return; // TODO: Throw error
+    if (giveaway.cancelledBy) {
+      description.push(`This giveaway has been **cancelled**.`);
+    } else {
+      description.push(`React with ${Utils.Emojis.TADA} to enter!`);
+
+      if (giveaway.winnersCount !== 1)
+        description.push(`Winners: **${giveaway.winnersCount}**`);
+
+      for (const requirement of giveaway.requirements) {
+        if (requirement.type === "GUILD") {
+          const requirementGuildDatabase = await Guild.findOne({
+            where: { id: requirement.target as string }
+          });
+          if (!requirement) return; // TODO: Throw error
+          description.push(
+            `Must join: **[${requirementGuildDatabase?.name}](${requirement.invite})**`
+          );
+        } else if (requirement.type === "ROLE") {
+          const role = guild.roles.fetch(requirement.target);
+          if (!role) return; // TODO: Throw error
+          description.push(`Must have role: ${role}`);
+        } else if (requirement.type === "VOTE")
+          description.push(
+            `Must vote for: **[${
+              OpenBump.instance.client.user?.username
+            }](${Utils.Lists.getLinkTopGG()})**`
+          );
+        else return; // TODO: Throw error
+      }
     }
+
+    const ended = giveaway.endedAt || giveaway.cancelledBy;
 
     const embed: MessageEmbedOptions = {
       color: Utils.Colors.GREEN,
       title: `${Utils.Emojis.GIFT} ${giveaway.prize}`,
       description: description.join("\n"),
       footer: {
-        text: `${giveaway.winnersCount} Winner${
-          giveaway.winnersCount === 1 ? "" : "s"
-        } | Ends at`
+        text: ended
+          ? `Giveaway ended`
+          : `${giveaway.winnersCount} Winner${
+              giveaway.winnersCount === 1 ? "" : "s"
+            } | Ends at`
       },
-      timestamp: Date.now() // TODO: Use correct date
+      timestamp: ended ? undefined : endsAt
     };
 
     return embed;
