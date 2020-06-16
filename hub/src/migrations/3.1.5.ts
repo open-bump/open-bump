363f7e89021318e9aec0ef1c408d494e62b65180
lyne <me@lyne.io>
2020-06-16 20:08:27 +0200

    Update migration engine

--- hub/src/migrations/3.1.5.ts
+++ hub/src/migrations/3.1.5.ts
@@ -1,6 +1,6 @@
-import { MigrationJS } from "./helpers/Migrator";
+import { Migratable } from "./helpers/Migrator";
 
-const migration: MigrationJS = {
+const migration: Migratable = {
   version: "3.1.5",
   up: async (connection, datatypes, op) => {
     const transaction = await connection.transaction();
