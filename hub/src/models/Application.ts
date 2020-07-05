bab404c3192188c48d62874ac75ea22c3212e839
lyne <me@lyne.io>
2020-07-05 15:30:20 +0200

    Add application models

---
+++ hub/src/models/Application.ts
@@ -0,0 +1,44 @@
+import {
+  AllowNull,
+  BelongsTo,
+  Column,
+  DataType,
+  Default,
+  ForeignKey,
+  HasMany,
+  Model,
+  PrimaryKey,
+  Table
+} from "sequelize-typescript";
+import ApplicationFeature from "./ApplicationFeature";
+import User from "./User";
+
+@Table({
+  tableName: "Application"
+})
+export default class Application extends Model<Application> {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  id!: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  name!: string;
+
+  @ForeignKey(() => User)
+  @Column
+  userId!: string;
+
+  @Default(DataType.UUIDV4)
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  token!: string;
+
+  @BelongsTo(() => User)
+  user!: User;
+
+  @HasMany(() => ApplicationFeature)
+  features!: Array<ApplicationFeature>;
+}
