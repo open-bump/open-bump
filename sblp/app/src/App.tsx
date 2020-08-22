3b99190307c5b9867795f63da6e862f501df644f
lyne <me@lyne.io>
2020-08-22 23:49:20 +0200

    Basic Structure

--- sblp/app/src/App.tsx
+++ sblp/app/src/App.tsx
@@ -1,7 +1,39 @@
+import { createMuiTheme, makeStyles, ThemeProvider } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex"
+  }
+}));
 
 function App() {
-  return <h1>Help</h1>;
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#90caf9"
+      }
+    }
+  });
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Layout>
+            <Home />
+          </Layout>
+        </BrowserRouter>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
